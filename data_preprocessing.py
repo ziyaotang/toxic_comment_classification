fef7da672dcad33e8e9806e8fb4c61918f050fde
mcdullna <tangziyaodonna@gmail.com>
2018-04-20 21:54:39 -0400

    style: no code change

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,22 +1,10 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from nltk.corpus import stopwords
 import string
-from collections import Counter
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Dense, Input, LSTM, Embedding, Dropout, Activation
-from keras.layers import Bidirectional, GlobalMaxPool1D
-from keras.models import Model, Sequential
-from keras import initializers, regularizers, constraints, optimizers, layers
-
-
-# load text data
-train = pd.read_csv('train.csv').fillna('')
-test = pd.read_csv('test.csv').fillna('')
-print('Trainning set contains {} records and testing set contains {} records.'.format(len(train),len(test)))
+from nltk.stem.wordnet import WordNetLemmatizer
 
 # function to preprocess text data
 def clean_doc(text_record):
@@ -34,16 +22,19 @@ def clean_doc(text_record):
     tokens = [token for token in tokens if token not in stop_words]
     # remove short words (one letter)
     tokens = [token for token in tokens if len(token) > 1]
+    # lemmatization
+    lem = WordNetLemmatizer()
+    tokens = [lem.lemmatize(token,"v") for token in tokens]
     sentence = ' '.join(tokens)
     return sentence
 
 # preprocess all comment texts in the trainning set and testing set
 train_text = train['comment_text'].copy()
 test_text = test['comment_text'].copy()
-for i in range(len(train_text)):
-    train_text[i] = clean_doc(train_text[i])
-for j in range(len(test_text)):
-    test_text[j] = clean_doc(test_text[j])
+
+train_text_clean = [clean_doc(comment) for comment in train_text]
+test_text_clean = [clean_doc(comment) for comment in test_text]
+
 
 # Compare the comment text after preprocessed
 print('The 0th comment text in trainning set is:')
@@ -54,37 +45,19 @@ print(train_text[0])
 # create a tokenizer
 max_features = 20000
 tokenizer = Tokenizer(num_words = max_features)
-# fit the tokenizer
 tokenizer.fit_on_texts(train_text)
-
-len(tokenizer.word_index)
-
-encoded_train = tokenizer.texts_to_sequences(train_text)
-encoded_train[0]
-
-n_words = [len(comment) for comment in encoded_train]
-plt.hist(n_words, bins = np.arange(0,410,10))
-plt.show()
+train_encoded = tokenizer.texts_to_sequences(train_text)
+val_encoded = tokenizer.texts_to_sequences(val_text)
+test_encoded = tokenizer.texts_to_sequences(test_text)
+# vocabulary size
+print('The vocabulary size is {}.'.format(len(tokenizer.word_index)))
+# examples
+print(train_encoded[0])
 
 # padding
-maxlen = 200
+maxlen = 250
 X_train = pad_sequences(train_encoded, maxlen = maxlen, padding = 'post')
-class_names = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
-y_train = y = train[class_names].values
-# build model
-X_train.shape
-model = Sequential()
-model.add(Embedding(max_features, 100, input_length=maxlen))
-model.add(LSTM(60, return_sequences=True,name='lstm_layer'))
-model.add(GlobalMaxPool1D())
-model.add(Dropout(0.1))
-model.add(Dense(50, activation="relu"))
-model.add(Dropout(0.1))
-model.add(Dense(6, activation="sigmoid"))
-#summary
-model.summary()
+X_test = pad_sequences(test_encoded, maxlen = maxlen, padding = 'post')
 
-model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
-batch_size = 32
-epochs = 1
-model.fit(X_train,y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+class_names = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+y_train = train[class_names].values
